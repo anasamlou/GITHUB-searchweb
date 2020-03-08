ba97b687583e6cf5d275bd4b7ccba27e3a9c0f25
anasamlou <61283934+anasamlou@users.noreply.github.com>
2020-03-08 20:40:37 +0100

    cambios

--- js/funciones.js
+++ js/funciones.js
@@ -16,4 +16,50 @@ url = "https://api.github.com/users/"+texto
         
                     $(".outname").html(text.login);
                     $(".followers").html(text.followers);
-        
+  }
+$(document).ready(function() {
+  $("#datos").hide();
+  $("#submit").click(function () {
+      $("#dataname").empty()
+      $("#dataid").empty()
+      $("#datafollowing").empty()
+      $("#datafollowers").empty()
+      $("#datalocation").empty()
+      $("#datamail").empty()
+      $("#foto").empty()
+      var contentToRemove = document.querySelectorAll("#lineatabla");
+      $(contentToRemove).remove();
+      $.get("https://api.github.com/users/" + $("#gitname").val(), function (data) {
+          $("#datos").show();0
+          $("#dataname").append(data.name)
+          $("#dataid").append(data.id)
+          $("#datafollowing").append(data.following)
+          $("#datafollowers").append(data.followers)
+          $("#datalocation").append(data.location)
+          $("#datamail").append(data.email)
+          $("#foto").attr("src",data.avatar_url)
+          $.get(data.repos_url, function (datarepos) {
+              var content = "<tbody>"
+              for(i=0; i<parseInt(data.public_repos); i++){
+                  content += '<tr id ="lineatabla"><td>' + datarepos[i].name + '</td>'+'<td>' + datarepos[i].language +'</td><td>' + datarepos[i].html_url +'</td></tr>';
+              }
+              content += "</tbody>"
+
+              $('#tablarepos').append(content);
+          });
+      });
+  });
+  $("#remove").click(function () {
+      $("#datos").hide();
+      $("#dataname").empty()
+      $("#dataid").empty()
+      $("#datafollowing").empty()
+      $("#datafollowers").empty()
+      $("#datalocation").empty()
+      $("#datamail").empty()
+      $("#foto").empty()
+      var contentToRemove = document.querySelectorAll("#lineatabla");
+      $(contentToRemove).remove();
+  });
+});
+
